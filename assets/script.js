4645d08741daaa9cfc3ad02cc15d87aa05cd5bce
Owen Dupree <odupree1988@gmail.com>
2022-02-14 09:54:53 -0500

    dynamic buttons added

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,23 @@
-timerEl = document.getElementById("timer");
+let timerEl = document.getElementById("timer");
+let startStopContainerEl = document.getElementById("start-stop-container");
+let resetContainerEl = document.getElementById("reset-container");
 
 var seconds = 0;
 var miliseconds = 0;
 
 let display = () => {
-  document.getElementById("start").addEventListener("click", () => {
+  startEl = document.createElement("button");
+  startEl.className = "btn btn-success";
+  startEl.setAttribute("id", "start");
+
+  startIconEl = document.createElement("i");
+  startIconEl.className = "bi bi-play-fill";
+  startIconEl.style.fontSize = "2rem";
+
+  startEl.append(startIconEl);
+  startStopContainerEl.append(startEl);
+
+  startEl.addEventListener("click", () => {
     time = setInterval(() => {
       if (miliseconds === 99) {
         miliseconds = 0;
@@ -14,18 +27,53 @@ let display = () => {
       }
       timerEl.innerText = seconds + ":" + miliseconds;
     }, 10);
+    startEl.remove();
+    changeIcon();
+    resetBtn();
   });
 
-  document.getElementById("stop").addEventListener("click", () => {
-    clearInterval(time);
-  });
+  let changeIcon = () => {
+    pauseEl = document.createElement("button");
+    pauseEl.className = "btn btn-success";
+    pauseEl.setAttribute("id", "stop");
 
-  document.getElementById("reset").addEventListener("click", () => {
-    clearInterval(time);
-    seconds = 0;
-    miliseconds = 0;
-    timerEl.innerText = seconds + "0:0" + miliseconds;
-  });
+    pauseIconEl = document.createElement("i");
+    pauseIconEl.className = "bi bi-pause-fill";
+    pauseIconEl.style.fontSize = "2rem";
+
+    pauseEl.append(pauseIconEl);
+    startStopContainerEl.append(pauseEl);
+
+    document.getElementById("stop").addEventListener("click", () => {
+      clearInterval(time);
+      pauseEl.remove();
+      resetEl.remove();
+      display();
+    });
+  };
+
+  let resetBtn = () => {
+    resetEl = document.createElement("button");
+    resetEl.className = "btn btn-danger";
+    resetEl.setAttribute("id", "reset");
+
+    resetIconEl = document.createElement("i");
+    resetIconEl.className = "bi bi-arrow-repeat";
+    resetIconEl.style.fontSize = "2rem";
+
+    resetEl.append(resetIconEl);
+    resetContainerEl.append(resetEl);
+
+    document.getElementById("reset").addEventListener("click", () => {
+      clearInterval(time);
+      seconds = 0;
+      miliseconds = 0;
+      timerEl.innerText = seconds + "0:0" + miliseconds;
+      resetEl.remove();
+      pauseEl.remove();
+      display();
+    });
+  };
 };
 
 display();
