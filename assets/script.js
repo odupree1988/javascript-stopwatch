cc5f84ed67b09fdebdf184e490ea139efefc6877
Owen Dupree <odupree1988@gmail.com>
2022-02-13 22:10:48 -0500

    start, stop, reset functionality

--- assets/script.js
+++ assets/script.js
@@ -3,15 +3,28 @@ timerEl = document.getElementById("timer");
 var seconds = 0;
 var miliseconds = 0;
 
-startTimer = () => {
-  setInterval(() => {
-    if (miliseconds === 99) {
-      miliseconds = 0;
-      seconds += 1;
-    }
-    miliseconds += 1;
-    timerEl.innerText = seconds + ":" + miliseconds;
-  }, 10);
+let display = () => {
+  document.getElementById("start").addEventListener("click", () => {
+    t = setInterval(() => {
+      if (miliseconds === 99) {
+        miliseconds = 0;
+        seconds += 1;
+      } else {
+        miliseconds += 1;
+      }
+      timerEl.innerText = seconds + ":" + miliseconds;
+    }, 0);
+  });
+
+  document.getElementById("stop").addEventListener("click", () => {
+    clearInterval(t);
+  });
+
+  document.getElementById("reset").addEventListener("click", () => {
+    seconds = 0;
+    miliseconds = 0;
+    timerEl.innerText = seconds + "0:0" + miliseconds;
+  });
 };
 
-startTimer();
+display();
